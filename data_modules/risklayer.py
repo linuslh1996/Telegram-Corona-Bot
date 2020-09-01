c0dc01a43a309d81a89e51053d44f03e1c3dfe6e
Linus Heinzl <linus.heinzl@student.hpi.de>
2020-09-01 13:25:46 +0200

    refactors to make some functions in module private

--- data_modules/risklayer.py
+++ data_modules/risklayer.py
@@ -19,15 +19,6 @@ class KreisInformation:
     bundesland: Optional[str]
     date: datetime.date
 
-def preprocess_raw_data(kreis_names_raw: List[List[str]], new_cases_today_raw: List[List[str]], contributors_raw: List[List[str]], links_raw: List[List[str]]) -> List[KreisInformation]:
-    new_cases_today: List[int] = [int(case_number[0].replace(" ", "")) for case_number in new_cases_today_raw]
-    kreis_names: List[str] = [kreis_name[0] for kreis_name in kreis_names_raw]
-    links: List[str] = [link[0] for link in links_raw]
-    kreis_is_already_entered: List[bool] = [len(contributor) != 0 for contributor in contributors_raw]
-    kreis_infos: List[KreisInformation] = [KreisInformation(i, name, is_entered, new_cases, link, None, datetime.date(datetime.today()))
-                                           for i, (name, is_entered, new_cases, link) in enumerate(zip(kreis_names, kreis_is_already_entered, new_cases_today, links))]
-    return kreis_infos
-
 def get_new_data(api_key: str) -> List[KreisInformation]:
     # Define Spreadsheet Url
     RISKLAYER_SPREADSHEET_ID = "1wg-s4_Lz2Stil6spQEYFdZaBEp8nWW26gVyfHqvcl8s"
@@ -39,15 +30,28 @@ def get_new_data(api_key: str) -> List[KreisInformation]:
     contributors_raw: List[List[str]] = requests.get(f'{spreadsheet_query_url}/Haupt!S6:S406', parameters).json()["values"]
     links_raw: List[List[str]] = requests.get(f'{spreadsheet_query_url}/Haupt!R6:R406', parameters).json()["values"]
     # Convert Data
-    kreis_infos: List[KreisInformation] = preprocess_raw_data(kreis_names_raw, new_cases_today_raw, contributors_raw, links_raw)
+    kreis_infos: List[KreisInformation] = _preprocess_raw_data(kreis_names_raw, new_cases_today_raw, contributors_raw, links_raw)
     return kreis_infos
+
 
 def get_all_kreise(postgres_db: PostgresDatabase) -> List[str]:
     sql: SQL = SQL("SELECT kreis FROM kreise")
     results = postgres_db.get(sql)
     return [result["kreis"] for result in results]
 
+
 def get_all_bundeslaender(postgres_db: PostgresDatabase) -> List[str]:
     sql: SQL = SQL("SELECT DISTINCT bundesland FROM kreise")
     results = postgres_db.get(sql)
     return [result["bundesland"] for result in results]
+
+
+def _preprocess_raw_data(kreis_names_raw: List[List[str]], new_cases_today_raw: List[List[str]], contributors_raw: List[List[str]], links_raw: List[List[str]]) -> List[KreisInformation]:
+    new_cases_today: List[int] = [int(case_number[0].replace(" ", "")) for case_number in new_cases_today_raw]
+    kreis_names: List[str] = [kreis_name[0] for kreis_name in kreis_names_raw]
+    links: List[str] = [link[0] for link in links_raw]
+    kreis_is_already_entered: List[bool] = [len(contributor) != 0 for contributor in contributors_raw]
+    kreis_infos: List[KreisInformation] = [KreisInformation(i, name, is_entered, new_cases, link, None, datetime.date(datetime.today()))
+                                           for i, (name, is_entered, new_cases, link) in enumerate(zip(kreis_names, kreis_is_already_entered, new_cases_today, links))]
+    return kreis_infos
+
